293d0c6195dc7b58628490d05f3c9406bba9000c
codehlb <2396018503@qq.com>
2023-03-08 18:58:06 +0800

    test.3_7.3

---
+++ test.3_7.3/src/com/Stu.java
@@ -0,0 +1,14 @@
+package com;
+
+public class Stu {
+    String name;
+    String id;
+    int nl;
+    String nu;
+    char sex;
+    void introduce{
+        System.out.println("我叫"+name);
+    }
+}
+
+
---
+++ test.3_7.3/src/com/test_Stu.java
@@ -0,0 +1,9 @@
+package com;
+
+public class test_Stu {
+    public static void main(String[] args) {
+        Stu stu1 = new Stu();
+        stu1.name = "xiaoming";
+        System.out.printf("%s",stu1.name);
+    }
+}
