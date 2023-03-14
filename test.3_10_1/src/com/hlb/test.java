7fc2c51d4effaa72590ac193431b7b3c038e3380
codehlb <2396018503@qq.com>
2023-03-14 17:35:42 +0800

    test.3_10_1

---
+++ test.3_10_1/src/com/hlb/test.java
@@ -0,0 +1,13 @@
+package com.hlb;
+
+public class test {
+//    public static void main(String[] args) {
+//
+//        int k=100;
+//        String str = String.valueOf(k);
+//
+//        System.out.printf("%s",k);
+//    }
+
+    
+}
