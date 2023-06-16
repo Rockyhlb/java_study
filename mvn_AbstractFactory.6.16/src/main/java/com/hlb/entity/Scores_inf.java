107b3b6504a0bcc7af503b7281e629f01509180a
codehlb <2396018503@qq.com>
2023-06-16 16:59:43 +0800

    mvn_AbstractFacctory.6.16  在之前抽象工厂的基础上添加了manven来管理jar包

---
+++ mvn_AbstractFactory.6.16/src/main/java/com/hlb/entity/Scores_inf.java
@@ -0,0 +1,11 @@
+package com.hlb.entity;
+
+import java.text.ParseException;
+
+public class Scores_inf extends AEntity {
+
+    @Override
+    void setEntity(Object object) throws ParseException {
+
+    }
+}
